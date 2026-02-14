c86c616f3a5ae5485884243fcff75526c7d0815f
harshvardhan0706 <harshvardhansingh3425@gmail.com>
2026-02-14 23:42:50 +0530

    Add comments to Redux store configuration

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,9 @@ import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './cartSlice';
 import productReducer from './productSlice';
 
+/**
+ * Configure Redux store with cart and product slices
+ */
 const store = configureStore({
   reducer: {
     cart: cartReducer,
